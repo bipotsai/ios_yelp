8818b799b5c85b1d4a28f69bc0e57acd1775de29
bipotsai <bipolet@gmail.com>
2015-06-21 14:15:10 +0800

    Initial Commit

---
+++ yelp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  yelp
+//
+//  Created by Bipo Tsai on 6/21/15.
+//  Copyright (c) 2015 Bipo Tsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
